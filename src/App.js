e4295ff34a39a19dc48f0955937d706c75b7c3d4
vigneshviky01 <vikycharu@gmail.com>
2024-10-05 21:45:30 +0530

    save

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+  <div>
+
+<p style={{ fontWeight: 'bold', fontSize: 52 }}>Helloooo, This is Vignesh 71772118152 from IT dept</p>
+</div>
+  );
+};
+
+export default App;
